a352d675cbb7b52aa4423fc1bc7d6b8c3ad88800
LEONARDO SILVA BARROS <agenciagnsis@gmail.com>
2024-10-29 22:44:37 -0300

    preloader.js

--- preloader.js
+++ preloader.js
@@ -18,32 +18,24 @@ lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
     max-width: 100%;
     max-height: 100%;
-    display: none;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 `;
-lottieContainer.style.display = 'none';
 
 document.body.appendChild(overlay);
 document.body.appendChild(lottieContainer);
 
 function hideOverlay() {
     overlay.style.display = 'none';
-    lottieContainer.style.display = 'block';
-    // Substitua pela URL direta do seu arquivo JSON do Lottie.
     lottie.loadAnimation({
         container: lottieContainer,
-        renderer: 'svg', // ou 'canvas' se preferir
+        renderer: 'svg',
         loop: true,
         autoplay: true,
         path: 'https://agenciagnsis.github.io/preloaderJSLottie/coracao.json',
     });
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    hideOverlay(); // Teste sem o setTimeout
-});
 
 window.addEventListener('load', hideOverlay);
